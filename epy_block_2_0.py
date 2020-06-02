f11eae13135d7d557fc20ccd22ae93529a948d2a
Rein Jaks <reinjaks@gmail.com>
2020-06-02 15:01:47 +0300

    Added direction finder GRC files

---
+++ epy_block_2_0.py
@@ -0,0 +1,36 @@
+"""
+Embedded Python Blocks:
+
+Each time this file is saved, GRC will instantiate the first class it finds
+to get ports and parameters of your block. The arguments to __init__  will
+be the parameters. All of them are required to have default values!
+"""
+
+import numpy as np
+from gnuradio import gr
+from scipy import stats
+
+class blk(gr.sync_block):  # other base classes are basic_block, decim_block, interp_block
+    """Embedded Python Block example - a simple multiply const"""
+
+    def __init__(self, vlen=128
+):  # only default arguments here
+        """arguments to this function show up as parameters in GRC"""
+        gr.sync_block.__init__(
+            self,
+            name='Embedded Python Block',   # will show up in GRC
+            in_sig=[(np.float32, vlen)],
+            out_sig=[np.float32]
+        )
+        # if an attribute with the same name as a parameter is found,
+        # a callback is registered (properties work, too).
+        
+
+    def work(self, input_items, output_items):
+        """example: multiply with constant"""
+        #k = stats.mean(np.array(input_items[0]))
+        k = np.average(np.array(input_items[0]))
+        #k = k[0]
+        #print(k[0][0][0])
+        output_items[0][:] = k[0][0][0]
+        return len(output_items[0])
